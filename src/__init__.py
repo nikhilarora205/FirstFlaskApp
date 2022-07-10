aa4fbe57a6ed9178d0598b138d4dc4daea2da033
Nikhil Arora <narora205@gmail.com>
2022-07-10 01:40:06 -0700

    setting up Flask project

---
+++ src/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+import os
+
+def create_app(test_config=None):
+
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+        app.config.from_mapping(
+            SECRET_KEY=os.environ.get("SECRET_KEY")
+        )
+    else:
+        app.config.from_mapping(test_config)
+
+    @app.get("/")
+    def index():
+        return "Hello world"
+
+
+    @app.get("/hello")
+    def hello_endpoint():
+        return {"message" : "Hello world"}
+
+    @app.get("/hello/")
+    def hello_endpoint_advanced():
+        return {"message" : "Hello world advanced!"}
+
+    return app 
+
+
+
