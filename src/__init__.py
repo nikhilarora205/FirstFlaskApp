96f8d25277f1619a8836afdcba00fdfdcb1b983a
Nikhil Arora <narora205@gmail.com>
2022-09-05 19:48:20 -0700

    added tokens for login

--- src/__init__.py
+++ src/__init__.py
@@ -8,6 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from src.auth import auth
 from src.bookmarks import bookmarks
 from src.database import db
+from flask_jwt_extended import JWTManager, jwt_manager
 
 def create_app(test_config=None):
 
@@ -16,13 +17,18 @@ def create_app(test_config=None):
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
-            SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DB_URI")
+            SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DB_URI"),
+            SQLALCHEMY_TRACK_MODIFICATIONS=False,
+            JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
         )
     else:
         app.config.from_mapping(test_config)
     
     db.app=app
     db.init_app(app)
+
+    JWTManager(app)
+
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
 
--- src/auth.py
+++ src/auth.py
@@ -1,11 +1,78 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from werkzeug.security import check_password_hash, generate_password_hash
+from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT
+import validators
+from src.database import User
+from flask_jwt_extended import create_access_token, create_refresh_token
 
 auth = Blueprint("auth", __name__, url_prefix="/api/v1/auth")
 
 
 @auth.post('/register') 
 def register():
-    return "User created"
+    username=request.json['username']
+    email = request.json['email']
+    password = request.json['password']
+    
+    if len(password) < 6:
+         return jsonify({'error': "Password is too short"}), HTTP_400_BAD_REQUEST
+   
+    if len(username) < 3:
+         return jsonify({'error': "User is too short"}), HTTP_400_BAD_REQUEST    
+
+    if not username.isalnum() or " " in username: 
+        return jsonify({'error': "Username should also be alphanumeric, also no space"}), HTTP_400_BAD_REQUEST
+    
+    if not validators.email(email):
+        return jsonify({'error': "Email is not valid"}), HTTP_400_BAD_REQUEST
+
+
+    if User.query.filter_by(email=email).first() is not None: 
+        return jsonify({'error': "Email is taken"}), HTTP_409_CONFLICT
+    
+    pwd_hash = generate_password_hash(password)
+
+    user = User(username = username, password =pwd_hash, email=email)
+    db.session.add(user)
+    db.session.commit()
+
+
+    
+
+    
+    return jsonify({
+        'message': "User created",
+        'user': {
+            'username': username, "email": email
+        }
+    }), HTTP_201_CREATED
+
+
+@auth.post('/login') 
+def login():
+    email = request.json.get('email', '')
+    password = request.json.get('password', '')
+
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        is_pass_correct = check_password_hash(user.password, password)
+
+        if is_pass_correct:
+            refresh=create_refresh_token(identity=user.id)
+            access = create_refresh_token(identity=user.id)
+
+            return jsonify({
+                'user':{
+                    'refresh': refresh,
+                    'access': access,
+                    'username': user.username,
+                    'email': user.email
+                }
+
+            }), HTTP_200_OK
+    return jsonify({'error': 'Wrong credentials'}), HTTP_401_UNAUTHORIZED
+
 
 @auth.get("/me")
 def me():
